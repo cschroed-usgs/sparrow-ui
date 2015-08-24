c69c06755e27061659610461a0122ce371006ae7
Ivan Suftin <isuftin@usgs.gov>
2015-08-24 07:03:49 -0500

    Some code cleanup

--- src/main/webapp/js/utils/spatialUtils.js
+++ src/main/webapp/js/utils/spatialUtils.js
@@ -1,44 +1,48 @@
+/*jslint browser: true */
+/*global define*/
 define([
+	'underscore',
 	'jquery',
 	'module'
-], function ($, module) {
+], function (_, $, module) {
 	"use strict";
 
 	var self = {};
+	var workspace = 'huc8-overlay';
 
 	self.GEOSERVER_ENDPOINT = module.config().endpointGeoserver;
 
 	self.getStatesForRegion = function (regionId, context) {
 		var deferred = $.Deferred();
 		$.ajax({
-			url: self.GEOSERVER_ENDPOINT + 'huc8-overlay/ows',
+			url: self.GEOSERVER_ENDPOINT + workspace + '/ows',
 			data: {
 				service: 'WFS',
 				version: '2.0.0',
 				request: 'GetFeature',
-				typeName: 'huc8-overlay:' + regionId,
+				typeName: workspace + ':' + regionId,
 				propertyName: 'States',
 				outputFormat: 'application/json'
 			},
-			context : {
-				that : context || this,
-				deferred : deferred
+			context: {
+				that: context || this,
+				deferred: deferred
 			},
 			success: function (data) {
 				var states = _.chain(data.features)
-					.map(function (feature) {
-						return feature.properties.States;
-					})
-					.map(function (states) {
-						return states.split(",");
-					})
-					.flatten()
-					.uniq()
-					.value();
-				
+						.map(function (feature) {
+							return feature.properties.States;
+						})
+						.map(function (states) {
+							return states.split(",");
+						})
+						.flatten()
+						.uniq()
+						.value();
+
 				this.deferred.resolveWith(this.that, [states]);
 			},
-			error : function () {
+			error: function () {
 				this.deferred.rejectWith(this.that, arguments);
 			}
 		});
