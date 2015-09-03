a861589856eef02d2a565090b6a1ca98b380ccbd
Mary Bucknell <mbucknell@usgs.gov>
2015-09-03 09:58:37 -0500

    The disambiguate modal now gets created when the map view is initialized. Added a show method that gets called rather than render. Show is called with the list of regions that will be shown in the modal.

--- src/main/webapp/js/views/DisambiguateRegionSelectionView.js
+++ src/main/webapp/js/views/DisambiguateRegionSelectionView.js
@@ -15,39 +15,46 @@ define([
 
 		render: function () {
 			var html = this.template(this.context);
-			this.$el.append(html);
+			this.$el.html(html);
 
 			this.modal = this.$el.modal(this.modalOptions);
 		},
 
 		/*
 		 * @param {Object} options
-		 *      @prop {Array of Object} regions - each object has an id and name property
 		 *      @prop {SelectionModel} selectionModel
 		 *      @prop {ModelCollection} collection
 		 *      @prop {Object} modalOptions - options to use when creating the modal window in this view.
 		 */
 		initialize: function (options) {
 			BaseView.prototype.initialize.apply(this, arguments);
-			this.context.regions = _.map(options.regions, function(r) {
-				return _.extend(r, {
-					constituents : this.collection.getConstituents(r.id)
-				});
-			}, this);
+			this.el = options.el;
+
 			this.selectionModel = options.selectionModel;
 			this.modalOptions = options.modalOptions || {};
 
 		},
 
+		/*
+		 * @param {Array of Object} regions - each object has an id and name property
+		 * Rerender and show the modal with regions.
+		 */
+		show : function(regions) {
+			this.context.regions = _.map(regions, function(r) {
+				return _.extend(r, {
+					constituents : this.collection.getConstituents(r.id)
+				});
+			}, this);
+			this.setElement($(this.el)).render();;
+		},
+
 		updateRegionSelection : function(evt) {
 			var regionId = $(evt.currentTarget).data('regionId');
 			this.selectionModel.set('region', regionId);
-			this.remove();
 		},
 
 		unsetRegionSelection : function() {
 			this.selectionModel.set('region', '');
-			this.remove();
 		}
 	});
 
