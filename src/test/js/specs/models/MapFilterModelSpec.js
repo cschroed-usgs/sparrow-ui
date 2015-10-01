8b4a7eb0f053390cb53d5c6b1b27a11526b8308e
Ivan Suftin <isuftin@usgs.gov>
2015-10-01 11:04:08 -0500

    Updated spec a bit for better testing

--- src/test/js/specs/models/MapFilterModelSpec.js
+++ src/test/js/specs/models/MapFilterModelSpec.js
@@ -8,8 +8,26 @@ define([
 		it("resets to the defaults", function () {
 			var model = new MapFilterModel();
 			var defaults = model.defaults();
-			model.reset();
+			
+			model.set('state', ['AK']);
+			model.set('waterBody', '...');
+			model.set('waterShed', '...');
+			model.set('dataSeries', '...');
+			model.set('groupResultsBy', '...');
+			model.set('waterSheds', [1]);
+			
 			var test = model.attributes;
+			expect(test.state).not.toBe(defaults.state);
+			expect(test.waterBody).not.toBe(defaults.waterBody);
+			expect(test.waterShed).not.toBe(defaults.waterShed);
+			expect(test.dataSeries).not.toBe(defaults.dataSeries);
+			expect(test.groupResultsBy).not.toBe(defaults.groupResultsBy);
+			expect(test.waterSheds.length).not.toBe(defaults.waterSheds.length);
+			
+			model.reset();
+			test = model.attributes;
+			
+			
 			
 			expect(test.state).toBe(defaults.state);
 			expect(test.waterBody).toBe(defaults.waterBody);
